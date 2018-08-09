52eaed0a57215a5a12badbcc44356d40f4a15196
litsungyi <litsungyi@sigono.com>
2018-08-09 11:19:49 +0800

    Add test scenes and scripts

---
+++ Assets/Scripts/TestMonoBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using Camus.Validators;
+using UnityEngine;
+
+namespace Test
+{
+   public class TestMonoBehaviour : MonoBehaviour
+	{
+        [NotNull, SerializeField] private GameObject notNullPrivateItem;
+		[NotNull, SerializeField] private GameObject notNullPrivateItem_Null;
+		[SerializeField] private GameObject privateItem;
+		[SerializeField] private GameObject privateItem_Null;
+		[NotNull] public GameObject notNullPublicItem;
+		[NotNull] public GameObject notNullPublicItem_Null;
+		public GameObject publicItem;
+		public GameObject publicItem_Null;
+    }
+}
